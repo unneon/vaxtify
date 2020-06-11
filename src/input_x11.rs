1d2b47bbf0f73a66473f1708f045de8efe3f0812
Mateusz Cegiełka <mateusz@cegla.net>
2020-06-11 13:25:33 +0200

    :sparkles: Add PID fetching

--- src/input_x11.rs
+++ src/input_x11.rs
@@ -1,8 +1,8 @@
-use std::ffi::CStr;
-use std::os::raw::{c_char, c_uint, c_void};
+use std::ffi::{CStr, CString};
+use std::mem::size_of;
+use std::os::raw::{c_char, c_int, c_uint, c_ulong, c_void};
 use std::ptr::{null, null_mut};
-
-use x11::xlib::{Display as RawDisplay, Window, XWindowAttributes};
+use x11::xlib::{AnyPropertyType, Atom, Display as RawDisplay, Success, Window, XGetAtomName, XWindowAttributes};
 
 pub struct Display(*mut RawDisplay);
 
@@ -10,6 +10,12 @@ pub struct QueryTree {
 	pub parent: Window,
 	pub root: Window,
 	pub children: Vec<Window>,
+}
+
+pub struct Property<T> {
+	pub kind: Atom,
+	pub kind_name: String,
+	pub items: Vec<T>,
 }
 
 impl Display {
@@ -56,6 +62,62 @@ impl Display {
 		}
 		let name = unsafe { xstr_into_string(name) };
 		Ok(name)
+	}
+
+	pub fn get_window_pid(&self, window: Window) -> Option<u32> {
+		let prop = self.get_window_property::<u32>(window, "_NET_WM_PID").unwrap()?;
+		assert_eq!(prop.kind_name, "CARDINAL");
+		assert_eq!(prop.items.len(), 1);
+		Some(prop.items[0])
+	}
+
+	pub fn get_window_property<T: Clone>(&self, window: Window, name: &str) -> Result<Option<Property<T>>, ()> {
+		let name = CString::new(name).unwrap();
+		let atom = unsafe { x11::xlib::XInternAtom(self.0, name.as_ptr(), x11::xlib::True) };
+		if atom == 0 {
+			return Err(());
+		}
+		let mut kind: Atom = 0;
+		let mut format: c_int = 0;
+		let mut items_len: c_ulong = 0;
+		let mut bytes: c_ulong = 0;
+		let mut data: *mut u8 = null_mut();
+		let status = unsafe {
+			x11::xlib::XGetWindowProperty(
+				self.0,
+				window,
+				atom,
+				0,
+				!0,
+				x11::xlib::False,
+				AnyPropertyType as c_ulong,
+				&mut kind,
+				&mut format,
+				&mut items_len,
+				&mut bytes,
+				&mut data,
+			)
+		};
+		if status != Success as i32 {
+			return Err(());
+		}
+		if kind == 0 {
+			return Ok(None);
+		}
+		assert_eq!(format as usize, 8 * size_of::<T>());
+		assert_eq!(bytes, 0);
+		let property = Property {
+			kind,
+			kind_name: self.get_atom_name(kind),
+			items: unsafe { xarray_into_vec::<T>(data as *mut T, items_len as c_uint) },
+		};
+		Ok(Some(property))
+	}
+
+	fn get_atom_name(&self, atom: Atom) -> String {
+		let raw = unsafe { XGetAtomName(self.0, atom) };
+		// FIXME: Check for error.
+		unsafe { xstr_into_string(raw) }
 	}
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+#![allow(unused)]
+
 mod input_x11;
 
 fn main() -> Result<(), ()> {
 	let display = input_x11::Display::open()?;
 	let tree = display.query_tree()?;
-	for child in tree.children {
-		let attributes = display.get_window_attributes(child)?;
-		let name = display.get_window_name(child);
-		println!("Found window called: {:?}\n{:#?}", name, attributes);
+	for window in tree.children {
+		println!("PID = {:?}", display.get_window_pid(window));
 	}
 	Ok(())
 }
