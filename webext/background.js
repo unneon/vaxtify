2c4051dd8b8ef05dd45799e6e5243268a78308ab
Mateusz Cegiełka <mateusz@cegla.net>
2020-06-12 14:07:44 +0200

    :sparkles: Add sending existing tabs to webext

--- webext/background.js
+++ webext/background.js
@@ -1,27 +1,47 @@
 let port = browser.runtime.connectNative("distraction_oni");
 
-browser.tabs.onCreated.addListener(tab => port.postMessage({
-    "kind": "Created",
-    "timestamp": new Date().toISOString(),
-    "tab": tab.id
-}));
-browser.tabs.onRemoved.addListener((tabId, removeInfo) => port.postMessage({
-    "kind": "Removed",
-    "timestamp": new Date().toISOString(),
-    "tab": tabId
-}));
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url !== undefined) {
+function on_created(tabId) {
+    port.postMessage({
+        "kind": "Created",
+        "timestamp": new Date().toISOString(),
+        "tab": tabId
+    });
+}
+function on_removed(tabId) {
+    port.postMessage({
+        "kind": "Removed",
+        "timestamp": new Date().toISOString(),
+        "tab": tabId
+    });
+}
+function on_updated(tabId, url) {
+    if (url !== undefined) {
         port.postMessage({
             "kind": "Updated",
             "timestamp": new Date().toISOString(),
             "tab": tabId,
-            "url": changeInfo.url
+            "url": url
         });
     }
+}
+function on_activated(tabId) {
+    port.postMessage({
+        "kind": "Activated",
+        "timestamp": new Date().toISOString(),
+        "tab": tabId
+    });
+}
+
+browser.tabs.onCreated.addListener(tab => on_created(tab.id));
+browser.tabs.onRemoved.addListener((tabId, removeInfo) => on_removed(tabId));
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => on_updated(tabId, changeInfo.url));
+browser.tabs.onActivated.addListener(activeInfo => on_activated(activeInfo.tabId));
+
+browser.tabs.query({}).then(tabs => {
+    for (let tab of tabs) {
+        on_created(tab.id);
+        on_updated(tab.id, tab.url);
+        if (tab.active)
+            on_activated(tab.id);
+    }
 });
-browser.tabs.onActivated.addListener(activeInfo => port.postMessage({
-    "kind": "Activated",
-    "timestamp": new Date().toISOString(),
-    "tab": activeInfo.tabId
-}));
